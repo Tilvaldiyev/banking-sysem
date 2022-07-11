718f0329b9ff0a2343f3c01fff3d8756c89b8683
Alisher Tilvaldiyev <tilvaldiyev@users.noreply.github.com>
2022-07-11 23:15:50 +0600

    load configs using viper

--- main.go
+++ main.go
@@ -3,19 +3,19 @@ package main
 import (
 	"banking-system/api"
 	"banking-system/db/sqlc"
+	"banking-system/util"
 	"database/sql"
 	_ "github.com/lib/pq"
 	"log"
 )
 
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgresql://root:fcrbgfhjkm@localhost:5432/banking_system?sslmode=disable"
-	serverAddress = "0.0.0.0:8080"
-)
-
 func main() {
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to DB:", err)
 	}
@@ -23,7 +23,7 @@ func main() {
 	store := sqlc.NewStore(conn)
 	server := api.NewServer(store)
 
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 	if err != nil {
 		log.Fatal("cannot start server:", err)
 	}
