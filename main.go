419d1d321dfab9f2c51e576ae71174e9eb62edba
Alisher Tilvaldiyev <tilvaldiyev@users.noreply.github.com>
2022-07-11 22:54:08 +0600

    create handlers

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"banking-system/api"
+	"banking-system/db/sqlc"
+	"database/sql"
+	_ "github.com/lib/pq"
+	"log"
+)
+
+const (
+	dbDriver      = "postgres"
+	dbSource      = "postgresql://root:fcrbgfhjkm@localhost:5432/banking_system?sslmode=disable"
+	serverAddress = "0.0.0.0:8080"
+)
+
+func main() {
+	conn, err := sql.Open(dbDriver, dbSource)
+	if err != nil {
+		log.Fatal("cannot connect to DB:", err)
+	}
+
+	store := sqlc.NewStore(conn)
+	server := api.NewServer(store)
+
+	err = server.Start(serverAddress)
+	if err != nil {
+		log.Fatal("cannot start server:", err)
+	}
+}
