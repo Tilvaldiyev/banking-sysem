262fd6dd2a36e2c98eb3e54eb2fba733454937cc
Alisher Tilvaldiyev <tilvaldiyev@users.noreply.github.com>
2022-07-16 12:54:38 +0600

    token generation added

--- api/server.go
+++ api/server.go
@@ -1,7 +1,10 @@
 package api
 
 import (
+	"fmt"
 	"github.com/Tilvaldiyev/banking-system/db/sqlc"
+	"github.com/Tilvaldiyev/banking-system/token"
+	"github.com/Tilvaldiyev/banking-system/util"
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/binding"
 	"github.com/go-playground/validator/v10"
@@ -9,20 +12,38 @@ import (
 
 // Server serves HTTP requests for our banking service
 type Server struct {
-	store sqlc.Store
-	router *gin.Engine
+	config util.Config
+	store      sqlc.Store
+	tokenMaker token.Maker
+	router     *gin.Engine
 }
 
 // NewServer creates a new HTTP server and setup routing
-func NewServer(store sqlc.Store) *Server {
-	server := &Server{store: store}
-	router := gin.Default()
+func NewServer(config util.Config, store sqlc.Store) (*Server, error) {
+	tokenMaker, err := token.NewPasetoMaker(config.TokenSymmetricKey)
+	if err != nil {
+		return nil, fmt.Errorf("cannot create token maker: %w", err)
+	}
+
+	server := &Server{
+		config: config,
+		store: store,
+		tokenMaker: tokenMaker,
+	}
 
 	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
 		v.RegisterValidation("currency", validCurrency)
 	}
 
+	server.setupRouter()
+	return server, nil
+}
+
+func (server *Server) setupRouter() {
+	router := gin.Default()
+
 	router.POST("/users", server.createUser)
+	router.POST("/users/login", server.loginUser)
 
 	router.POST("/accounts", server.createAccount)
 	router.GET("/accounts/:id", server.getAccount)
@@ -32,7 +53,6 @@ func NewServer(store sqlc.Store) *Server {
 
 	// add routes to router
 	server.router = router
-	return server
 }
 
 // Start runs the HTTP server on a specific address
